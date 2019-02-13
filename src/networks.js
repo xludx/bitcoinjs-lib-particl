1a3c6af8a95ded2afe8b1e83cf2f9f3614c9c7c5
Juha Kovanen <juha.kovanen@gmail.com>
2019-02-13 11:08:05 -0500

    particl

--- src/networks.js
+++ src/networks.js
@@ -34,5 +34,66 @@ module.exports = {
     pubKeyHash: 0x6f,
     scriptHash: 0xc4,
     wif: 0xef
-  }
+  },
+  litecoin: {
+    messagePrefix: '\x19Litecoin Signed Message:\n',
+    bip32: {
+      public: 0x019da462,
+      private: 0x019d9cfe
+    },
+    pubKeyHash: 0x30,
+    scriptHash: 0x05,
+    wif: 0xb0
+  },
+  dogecoin: {
+    messagePrefix: '\x19Dogecoin Signed Message:\n',
+    bip32: {
+      public: 0x02facafd,
+      private: 0x02fac398
+    },
+    pubKeyHash: 0x1e,
+    scriptHash: 0x16,
+    wif: 0x9e
+  },
+  dash: {
+    messagePrefix: '\x19DarkCoin Signed Message:\n',
+    bip32: {
+      public: 0x02fe52f8,
+      private: 0x02fe52cc
+    },
+    pubKeyHash: 0x4c,
+    scriptHash: 0x10,
+    wif: 0xcc
+  },
+  dash_testnet: {
+    messagePrefix: '\x19DarkCoin Signed Message:\n',
+    bip32: {
+      public: 0x3a8061a0,
+      private: 0x3a805837
+    },
+    pubKeyHash: 0x8b,
+    scriptHash: 0x13,
+    wif: 0xef
+  },
+  particl: {
+    messagePrefix: '\x19Particl Signed Message:\n',
+    bip32: {
+      public: 0x696e82d1,
+      private: 0x8f1daeb8
+    },
+    pubKeyHash: 0x38,
+    scriptHash: 0x3c,
+    wif: xxxx
+  },
+  particl_testnet: {
+    messagePrefix: '\x19Particl Signed Message:\n',
+    bip32: {
+      public: 0xe1427800,
+      private: 0x04889478
+    },
+    pubKeyHash: 0x76,
+    scriptHash: 0x7a,
+    wif: xxxx
+  },
+
 }
